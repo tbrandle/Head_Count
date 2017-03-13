282bf3aff6c02d852c4dee8bf83ac57c6f83fab6
Kyle Zucker <zkyle12@gmail.com>
2017-03-13 15:40:46 -0600

    complete iteration 1 tests

--- src/helper.js
+++ src/helper.js
@@ -2,12 +2,34 @@ export default class DistrictRepository {
   constructor (data) {
     this.data = data.reduce((acc, obj) => {
       if (!acc[obj.Location]) {
-        acc[obj.Location] =[]
+        acc[obj.Location] = []
       }
       acc[obj.Location].push(obj)
       return acc
     }, {})
   }
 
+  findByName (nameToFind) {
+    if (nameToFind) {
+      const key = Object.keys(this.data).find(key => nameToFind.toLowerCase() === key.toLowerCase())
+      if(key) {
+        const dateScoreData = this.data[key].reduce((result, current) => {
+          if(typeof current.Data === "number"){
+            result[current.TimeFrame] = current.Data.toFixed(3) * 1
+          } else {
+            result[current.TimeFrame] = 0
+          }
+          return result
+        }, {})
+        return { location:key, data:dateScoreData }
+      }
+    }
 
+    // const results = Object.keys(this.data).filter((key) => {
+    //   return location.toLowerCase() === key.toLowerCase()
+    // })
+    //{location: ??, data: { "2004": 0.302, "2005": 0.267...}}
+  }
 }
+
+
