b38f0572ea3d6b968bed0c39cf6d737c80ffa16f
Tim <tbrandle53@gmail.com>
2017-03-14 15:33:55 -0600

    Iteration 4 tests pass. Add compareAverage function

--- src/helper.js
+++ src/helper.js
@@ -32,4 +32,29 @@ export default class DistrictRepository {
          ? Object.keys(this.data).filter(key => key.toLowerCase().includes(nameToFind.toLowerCase()))
          : Object.keys(this.data);
   }
+
+  findAverage(nameToFind) {
+    const location = this.findByName(nameToFind);
+    const sum = Object.keys(location.data).reduce((acc, key) => {
+      return location.data[key] + acc
+    },0)
+    const totalAverage = sum / Object.keys(location.data).length
+    return totalAverage.toFixed(3) * 1
+  }
+
+  compareDistrictAverages(district1, district2){
+    const averageOne = this.findAverage(district1)
+    const averageTwo = this.findAverage(district2)
+    let compared
+    if (averageOne < averageTwo) {
+       compared = averageOne / averageTwo
+    } else {
+       compared =  averageTwo / averageOne
+    }
+    compared = compared.toFixed(3)*1
+    const obj = { compared }
+    obj[district1] = averageOne
+    obj[district2] = averageTwo
+    return obj;
+  }
 }
--- test/unit/iteration-4.test.js
+++ test/unit/iteration-4.test.js
@@ -22,7 +22,7 @@ describe.skip('DistrictRepository iteration 0', () =>  {
 
   test('compareDistrictAverages ACADEMY 20 against Colorado', () => {
     const result = { "ACADEMY 20": 0.407, "COLORADO": 0.53, "compared": 0.768}
-    expect(district.compareDistrictAverages('ACADEMY 20', 'Colorado')).toEqual(result);
+    expect(district.compareDistrictAverages('Colorado', 'ACADEMY 20')).toEqual(result);
   });
 
 });
