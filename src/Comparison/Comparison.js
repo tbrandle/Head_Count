4e6685a1fc1a1120afce6fcccd9bc2d48ce770dc
Tim <tbrandle53@gmail.com>
2017-03-15 16:24:44 -0600

    Flex Comparison...add clear button...needs functionality

--- src/Comparison/Comparison.js
+++ src/Comparison/Comparison.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import DistrictCard from '../DistrictCard/DistrictCard';
 
+import './Comparison.css';
+import '../DistrictCard/DistrictCard.css';
+
 
 class Comparison extends Component {
   constructor () {
@@ -11,14 +14,17 @@ class Comparison extends Component {
 
     if (this.props.card1 && !this.props.card2) {
       return (
-        <DistrictCard districtData={this.props.card1}/>
+        <div className='comparison'>
+          <DistrictCard districtData={this.props.card1}/>
+        </div>
+
       )
     } else if (this.props.card1 && this.props.card2) {
       const compareObject = this.props.repo.compareDistrictAverages(this.props.card1.location, this.props.card2.location)
       return (
-        <div>
+        <div className='comparison'>
           <DistrictCard districtData={this.props.card1}/>
-          <div>
+          <div className='district-card'>
             { compareObject.compared }
             { this.props.card1.location }
             { compareObject[this.props.card1.location] }
@@ -27,6 +33,7 @@ class Comparison extends Component {
 
           </div>
           <DistrictCard districtData={this.props.card2}/>
+          <button type='submit' value='clear'>clear</button>
         </div>
       )
     }
