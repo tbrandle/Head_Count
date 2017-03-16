acc6f732c41f0ae35e2e947989f5cc3ffbdddb97
Tim <tbrandle53@gmail.com>
2017-03-16 14:58:17 -0600

    Change DistrictCard from class to const

--- src/DistrictCard/DistrictCard.js
+++ src/DistrictCard/DistrictCard.js
@@ -1,28 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './DistrictCard.css';
 
 
-class DistrictCard extends Component {
-  render () {
+const checkPercentage = (districtData, key) => {
+  if (districtData.data[key] < .50) {
+    return 'percentage-low'
+  } else {
+    return 'percentage-high'
+  }
+}
+
+const DistrictCard = ({ districtData, selectMe }) => {
+
     return (
-      <div className='district-card' onClick={(e) => this.props.selectMe(this)}>
-      <h3 className='title' >{this.props.districtData.location}</h3>
-      <section className='data-section' >
-      { Object.keys(this.props.districtData.data).map((key, i) =>
-        <p className={ this.checkPercentage(key) } key={i}> { key } { this.props.districtData.data[key] } </p>
-      )}
-      </section>
+      <div className='district-card' onClick={(e) => selectMe()}>
+        <h3 className='title' >{districtData.location}</h3>
+        <section className='data-section' >
+        { Object.keys(districtData.data).map((key, i) =>
+          <p className={ checkPercentage(districtData, key) } key={i}> { key } { districtData.data[key] } </p>
+        )}
+        </section>
       </div>
     )
-  }
 
-  checkPercentage(key){
-    if (this.props.districtData.data[key] < .50) {
-      return 'percentage-low'
-    } else {
-      return 'percentage-high'
-    }
-  }
 }
 
 
